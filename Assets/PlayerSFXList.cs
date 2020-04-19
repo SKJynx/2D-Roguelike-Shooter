04f3de3f0dca214893d93e14ac044f691a319361
SKJynx <SK_Jynx@hotmail.com>
2020-04-18 21:21:20 -0400

    Updated PlayerSFX List
    Added callbacks for FMOD, and enabled a custom struct array to tell what sound is being called.

--- Assets/PlayerSFXList.cs
+++ Assets/PlayerSFXList.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class PlayerSFXList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // Enable referencing of sound effects for EventRef
+    public MyStruct[] structArray;
+    [System.Serializable]
+    public struct MyStruct
     {
-        
+        public string title;
+        public int number;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    [FMODUnity.EventRef]
+    public string[] soundEffect;
+
+    public void FootstepSFX()
     {
-        
+        FMODUnity.RuntimeManager.PlayOneShot(soundEffect[0]);
     }
+
 }
